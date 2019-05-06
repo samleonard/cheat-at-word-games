c18fcf338d129c90ea58ac6b6546f41d7cea4807
Samuel Leonard <sam.leonard@amobee.com>
2019-05-06 16:09:07 -0700

    Second commit

--- filter_sowpods.py
+++ filter_sowpods.py
@@ -1,31 +1,31 @@
 def create_spelling_bee_file():
+    def spelling_bee_filter(line):
+        if len(line.strip()) < 4:
+            return False
+        return True
+
     with open('sowpods.txt') as infile:
         lines = infile.readlines()
     with open('sowpods-spelling-bee.txt', 'w') as outfile:
-        for line in lines:
-            if len(line.strip()) >= 4:
-                outfile.write(line)
+        outfile.writelines(filter(spelling_bee_filter, lines))
 
 
-def create_crossroads_file():
-    with open('sowpods.txt') as infile:
-        lines = infile.readlines()
-    lines = [line for line in lines if len(line.strip()) >= 3]
-
-    def crossroads_filter(line):
+def create_letter_boxed_file():
+    def letter_boxed_filter(line):
         if len(line.strip()) < 3:
             return False
 
-        for i in range(1, len(s)):
-            if s[i] == s[i-1]:
+        for i in range(1, len(line)):
+            if line[i] == line[i-1]:
                 return False
 
         return True
-    lines = filter(no_double_letters, lines)
 
-    with open('sowpods-crossroads.txt', 'w') as outfile:
-        outfile.writelines(lines)
+    with open('sowpods.txt') as infile:
+        lines = infile.readlines()
+    with open('sowpods-letter-boxed.txt', 'w') as outfile:
+        outfile.writelines(filter(letter_boxed_filter, lines))
 
 
 if __name__ == '__main__':
-    create_crossroads_file()
+    create_letter_boxed_file()
