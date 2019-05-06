a569d61088101fb9dc46cec6fe248407917be5c1
Samuel Leonard <sam.leonard@amobee.com>
2019-05-06 16:59:30 -0700

    Implement cheat for letter boxed

--- cheat_letter_boxed.py
+++ cheat_letter_boxed.py
@@ -1,5 +1,50 @@
+import sys
+
+
+# might not work if there are duplicate letters
+def check_word(sides, line):
+    prev = -1
+    for letter in line.strip():
+        valid = False
+        for i in range(len(sides)):
+            # can't reuse side consecutively
+            if i == prev:
+                continue
+            if letter in sides[i]:
+                prev = i
+                valid = True
+                break
+        if not valid:
+            return False
+    return True
+
+
 def main():
-    pass
+    sides = ['PDS', 'LEI', 'CMO', 'RTU']
+    if len(sys.argv) == 5:
+        sides = sys.argv[1:]
+
+    with open('sowpods-letter-boxed.txt') as infile:
+        lines = infile.readlines()
+
+    lines = [line.strip() for line in lines if check_word(sides, line)]
+
+    index = {}
+
+    for line in lines:
+        index.setdefault(line[0], []).append(line)
+
+    letters = ''.join(sides)
+
+    for line in lines:
+        unseen = [letter for letter in letters if letter not in line]
+        for word in index[line[-1]]:
+            valid = True
+            for letter in unseen:
+                if letter not in word:
+                    valid = False
+            if valid:
+                print(line, word)
 
 
 if __name__ == '__main__':
