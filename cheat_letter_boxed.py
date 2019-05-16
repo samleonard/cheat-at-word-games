1451e68fdedc7ca49bdf7236dc98355ea38085b0
Samuel Leonard <sam.leonard@amobee.com>
2019-05-16 12:50:46 -0700

    Highlight solutions of interest for letter boxed

--- cheat_letter_boxed.py
+++ cheat_letter_boxed.py
@@ -27,24 +27,52 @@ def main():
     with open('sowpods-letter-boxed.txt') as infile:
         lines = infile.readlines()
 
+    # make a list of valid words
     lines = [line.strip() for line in lines if check_word(sides, line)]
 
     index = {}
 
+    # index all words by first letter
     for line in lines:
         index.setdefault(line[0], []).append(line)
 
     letters = ''.join(sides)
 
+    # find all 1 word solutions
+    singles = set()
     for line in lines:
+        if all(letter in line for letter in letters):
+            singles.add(line)
+
+    # find all 2 word solutions
+    answers = []
+    for line in lines:
+        if line in singles:
+            continue
+
         unseen = [letter for letter in letters if letter not in line]
+
         for word in index[line[-1]]:
-            valid = True
-            for letter in unseen:
-                if letter not in word:
-                    valid = False
-            if valid:
-                print(line, word)
+            if word in singles:
+                continue
+
+            if all(letter in word for letter in unseen):
+                answers.append((line, word))
+
+    shortest = min(len(''.join(answer)) for answer in answers)
+    shorties = []
+    for answer in answers:
+        if len(''.join(answer)) == shortest:
+            shorties.append(answer)
+
+    for answer in answers:
+        print(answer)
+    print('\n\n\n************TERSEST************')
+    for answer in shorties:
+        print(answer)
+    print('\n\n\n************SINGLES************')
+    for answer in singles:
+        print(answer)
 
 
 if __name__ == '__main__':
